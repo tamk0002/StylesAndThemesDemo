d9f878ab6f161d960a04a49fd3aeb8dff8f8d1a0
tamk0002 <Christine@Christines-MacBook-Pro.local>
2017-10-20 16:46:01 -0400

    initial commit

---
+++ app/src/main/java/com/algonquincollege/tamk0002/stylesandthemesdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.algonquincollege.tamk0002.stylesandthemesdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button);
+    }
+}
